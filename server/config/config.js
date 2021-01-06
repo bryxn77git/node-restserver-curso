4dc16e19f6ef86481da4d9c5bfa35b1017558723
BryanBalderrama <bryan.77@hotmail.com>
2021-01-06 12:44:34 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,8 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://bbalderrama:Side244889@cluster0.onyle.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
-//mongodb+srv://bbalderrama:Side244889@cluster0.onyle.mongodb.net/cafe?retryWrites=true&w=majority
